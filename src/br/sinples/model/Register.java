0055343a939067bac406a2703a5aab6922220c7b
Camilo Peric <cperic@egov.br>
2014-01-07 19:58:11 -0200

    First commit. This is just a skeleton.

---
+++ src/br/sinples/model/Register.java
@@ -0,0 +1,5 @@
+package br.sinples.model;
+
+public class Register {
+
+}
---
+++ src/br/sinples/wsi/IdentityWSI.java
@@ -0,0 +1,15 @@
+package br.sinples.wsi;
+
+import br.sinples.model.Register;
+
+public class IdentityWSI {
+
+	public boolean login(String username, String password) {
+		return false;
+	}
+	
+	public boolean register(Register register) {
+		return false;
+	}
+	
+}
